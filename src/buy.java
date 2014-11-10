2c5fee2f83577d54ad5f3c20c82276a9d1af0425
Thanos Korakas <thanoskorakas@gmail.com>
2014-11-10 17:28:36 +0200

    Add products to list and remake buy form

--- src/buy.java
+++ src/buy.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -14,24 +15,22 @@ import javax.swing.JTextField;
 public class buy extends JFrame {
 	JFrame j = new JFrame();
 	supplies sp = new supplies("test");
-	JTextField empid = new JTextField();
 	JTextField empname = new JTextField();
 	JTextField empsalary = new JTextField();
-	JLabel empnameL = new JLabel("Name");
-	JLabel empidL = new JLabel("ID");
-	JLabel empsalaryL = new JLabel("Salary");
+	JLabel empnameL = new JLabel("Product");
+	JLabel empsalaryL = new JLabel("Quantity");
 	JButton buyI = new JButton("Buy");
 	JLabel empty = new JLabel();
+	public static String[] list ;
 	//String[] list = { "ID", "Name", "Quantity" , "Date","Type","Price","FPA" };
-	JComboBox itemList = new JComboBox();
+	@SuppressWarnings("unchecked")
+	JComboBox itemList = new JComboBox(list);
 	public buy() throws SQLException {
 		j.setTitle("Buy");
-		j.setSize(200, 150);
-		j.setResizable(false);
+		j.setSize(500,120);
+		j.setResizable(true);
 		j.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		j.setLayout(new GridLayout(4, 2));
-		j.add(empidL);
-		j.add(empid);
+		j.setLayout(new GridLayout(3, 2));
 		j.add(empnameL);
 		j.add(itemList);
 		j.add(empsalaryL);
@@ -40,5 +39,6 @@ public class buy extends JFrame {
 		j.add(buyI);
 		j.setVisible(true);
 		
-	}
+	}//constructor
+	
 }
--- src/supplies.java
+++ src/supplies.java
@@ -11,20 +11,21 @@ import java.sql.SQLException;
 public class supplies extends JFrame {
 	/**
 	 */
-	//Database Variables
+	// Database Variables
 	public static Connection con;
 	public static java.sql.Statement pst = null;
 	public static java.sql.ResultSet rs;
-	//Table Data
-	private String[] columnNames = { "ID", "Name", "Quantity" , "Date","Type","Price","FPA" };
+	// Table Data
+	private String[] columnNames = { "ID", "Name", "Quantity", "Date", "Type",
+			"Price", "FPA" };
 	private static Object[][] data = new Object[100][7];
-	//GUI Variables
+	// GUI Variables
 	JTable table = new JTable(data, columnNames);
 	JScrollPane container = new JScrollPane(table);
 	JButton newIt = new JButton("Products"); // Products
 	JButton buyButton = new JButton("Buy"); // Buy
 	JButton sellButton = new JButton("Sell"); // Sell
-	//Resize table variables
+	// Resize table variables
 	TableColumn column1 = null;
 	TableColumn column2 = null;
 	TableColumn column3 = null;
@@ -32,9 +33,10 @@ public class supplies extends JFrame {
 	TableColumn column5 = null;
 	TableColumn column6 = null;
 	TableColumn column7 = null;
-	//constructor
+	private static int counter = 0;
+	// constructor
 	public supplies() throws SQLException {
-		//Creating window
+		// Creating window
 		JFrame frame = new JFrame();
 		frame.setTitle("Supplies");
 		frame.setSize(700, 600);
@@ -42,14 +44,14 @@ public class supplies extends JFrame {
 		frame.setResizable(false);
 		frame.setLayout(null);
 		frame.add(newIt);
-		//Set custom layout
+		// Set custom layout
 		container.setBounds(0, 30, 695, 590);
 		newIt.setBounds(0, 0, 150, 30);
 		frame.add(buyButton);
 		buyButton.setBounds(275, 0, 150, 30);
 		frame.add(sellButton);
 		sellButton.setBounds(550, 0, 150, 30);
-		//Resizing columns
+		// Resizing columns
 		column1 = table.getColumnModel().getColumn(0);
 		column1.setPreferredWidth(10);
 		column2 = table.getColumnModel().getColumn(1);
@@ -62,9 +64,8 @@ public class supplies extends JFrame {
 		column6.setPreferredWidth(30);
 		column7 = table.getColumnModel().getColumn(6);
 		column7.setPreferredWidth(30);
-		
-		
-		//onClick method for Products
+
+		// onClick method for Products
 		newIt.addActionListener(new ActionListener() {
 
 			@Override
@@ -72,11 +73,13 @@ public class supplies extends JFrame {
 				newItem ni = new newItem();
 			}
 		});
+		//Buy button
 		buyButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				try {
+					addData();
 					buy b = new buy();
 				} catch (SQLException e) {
 					e.printStackTrace();
@@ -94,11 +97,13 @@ public class supplies extends JFrame {
 		table.setEnabled(false);
 		frame.setVisible(true);
 	}// supplies
-	//constructor with parameter
+		// constructor with parameter
+
 	public supplies(String dif) {
 
 	}
-	//Connect to Database 
+
+	// Connect to Database
 	public static void connect() {
 		// insert library
 		try {
@@ -118,14 +123,12 @@ public class supplies extends JFrame {
 		}
 
 	}// Connect method
-	
-	
-	//Take Data from Database
+
+	// Take Data from Database
 	public static void Tablequery() throws SQLException {
 		rs = pst.executeQuery("select * from prods");
 		int i = 0;
 		while (rs.next()) {
-			// { "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) }
 
 			data[i][0] = rs.getString(1);
 			data[i][1] = rs.getString(2);
@@ -135,7 +138,18 @@ public class supplies extends JFrame {
 			data[i][5] = rs.getString(6);
 			data[i][6] = rs.getString(7);
 			i++;
-		}//query
+			counter++;
+		}// while
+	}// query
+	//Find all product names
+	public static void addData() throws SQLException {
+		rs = pst.executeQuery("select pname from prods;");
+		buy.list = new String[counter];
+		int i = 0;
+		while (rs.next()) {
+			buy.list[i] = rs.getString(1);
+			i++;
+		}// while
+		
 	}
-
 }
